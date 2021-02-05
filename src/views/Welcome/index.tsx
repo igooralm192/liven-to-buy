e919739d70154d2f1fe5ade032c77b9d0f1fd121
Igor de Almeida <igoor1205@gmail.com>
2021-02-05 15:27:47 -0300

    :pushpin: Create 'Button' component

--- src/views/Welcome/index.tsx
+++ src/views/Welcome/index.tsx
@@ -4,29 +4,26 @@ import { Link } from 'react-router-dom'
 import './styles.css'
 
 import Logo from '../../assets/images/logo.svg'
+import Button from '../../components/Button'
 
 const Welcome: React.FC = () => {
   return (
-    <section className="welcome container">
-      <div className="welcome title">
+    <section id="welcome-container">
+      <div id="welcome-title">
         Welcome to
         <img src={Logo} alt="Logo" />
       </div>
-      <div className="welcome description">
+      <div id="welcome-description">
         This is Liven&apos;s virtual store, take a look at our products and
         guarantee the best offers!
       </div>
 
-      <div className="welcome actions">
+      <div id="welcome-actions">
         <Link to="/sign-in">
-          <button type="button" className="welcome action">
-            Sign In
-          </button>
+          <Button className="welcome-action">Sign In</Button>
         </Link>
         <Link to="/sign-up">
-          <button type="button" className="welcome action">
-            Sign Up
-          </button>
+          <Button className="welcome-action">Sign Up</Button>
         </Link>
       </div>
     </section>
